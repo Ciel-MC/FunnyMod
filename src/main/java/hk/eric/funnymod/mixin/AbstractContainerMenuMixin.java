123a16c6d3c28810bddbcbf94503f5be1d62c885
eric <47144701+Shuaiouke@users.noreply.github.com>
2022-03-03 17:11:51 +0800

    Removed an empty mixin

--- src/main/java/hk/eric/funnymod/mixin/AbstractContainerMenuMixin.java
+++ src/main/java/hk/eric/funnymod/mixin/AbstractContainerMenuMixin.java
@@ -1,17 +1,8 @@
 package hk.eric.funnymod.mixin;
 
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ClickType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerMenu.class)
 public abstract class AbstractContainerMenuMixin {
-    @Inject(method = "clicked", at = @At("HEAD"))
-    public void injectClicked(int i, int j, ClickType clickType, Player player, CallbackInfo ci) {
-
-    }
 }
